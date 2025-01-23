bf108fec56c7240d38be9c7c631bcddaeedd8340
Jun Kyung Lee <junlee0325@gmail.com>
2025-01-23 06:35:05 -0500

    Feature/Generate Menu page, Calendar week select

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { WeeklyMenu } from "../components/weekly-menu/WeeklyMenu";
+import { NavLink } from "react-router-dom";
+
 
 export default function Home() {
     return (
-        <>
-            <button className="flex items-center ml-auto bg-slate-300 hover:bg-slate-200 text-black my-5 py-2 px-6 rounded-full">
-                <span className="mr-3">+</span> Generate Menu
-            </button>
+        <div className="flex flex-col items-center">
+            <div className="flex flex-row w-full justify-end">
+                <NavLink className="bg-blue-400 rounded-3xl p-3 m-3 w-1/6 text-center  hover:bg-gray-200" to={"/GenerateMenu"}>+ Generate Menu</NavLink>
+            </div>
             {localStorage.getItem("safeDishes") ? (
                 <div className="flex flex-col gap-10">
                     {/* Current Week menu */}
@@ -17,6 +19,6 @@ export default function Home() {
             ) : (
                 <p className="text-center">Set employee allergies before continuing</p>
             )}
-        </>
+        </div>
     );
 }
