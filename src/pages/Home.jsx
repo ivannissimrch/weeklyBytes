b1e0e28e42b932f2a9174a7f3eda19e1e71081a9
Jon <jcadeichmann@gmail.com>
2025-01-31 16:32:41 -0600

    Add success and error toast to download feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import { WeeklyMenu } from "../components/weekly-menu/WeeklyMenu";
 import { NavLink } from "react-router-dom";
 import { useEffect } from "react";
-import FileUploadIcon from '@mui/icons-material/FileUpload';
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 import moment from "moment";
 import useGenerateWeeklyDishes from "../hooks/useGenerateWeeklyDishes";
 import jsPDF from "jspdf";
-import { toast } from "react-toastify";
+import { Slide, ToastContainer, toast } from "react-toastify";
 
 export default function Home() {
     const [menuDishes, setMenuDishes] = useGenerateWeeklyDishes();
@@ -16,37 +16,44 @@ export default function Home() {
     }, []);
 
     const downloadMenuPDF = () => {
+        const showSuccess = () =>
+            toast.success("Your menu is downloading!", {
+                theme: "colored",
+            });
+        const showError = () =>
+            toast.error("There was an error. Please try again!", {
+                theme: "colored",
+            });
         try {
             const doc = new jsPDF();
             doc.setFont("helvetica", "bold");
             doc.setFontSize(16);
             doc.text("Weekly Menu", 16, 20);
-    
+
             const formatMenuData = (weekData, title, yOffset) => {
                 doc.setFont("helvetica", "bolditalic");
                 doc.setFontSize(12);
                 doc.text(title, 15, yOffset);
-                doc.setFont("helvetica", "normal"); 
+                doc.setFont("helvetica", "normal");
                 doc.setFontSize(12);
-    
+
                 weekData.forEach((dish, index) => {
                     const day = moment().startOf("isoWeek").add(index, "days").format("dddd");
                     const dishName = dish ? dish.name : "Day Off";
                     doc.text(`• ${day}: ${dishName}`, 15, yOffset + (index + 1) * 10);
                 });
             };
-    
+
             formatMenuData(menuDishes.currentWeek, "Current Week", 30);
             formatMenuData(menuDishes.nextWeek, "Upcoming Week", 120);
             doc.save("WeeklyMenu.pdf");
-            toast.success("PDF downloaded successfully!");
-    
+            showSuccess();
         } catch (error) {
             console.error("PDF Generation Error:", error);
-            toast.error("An error occurred while downloading the PDF.");
+            showError();
         }
     };
-    
+
     return (
         <div className="flex flex-col items-center">
             <div className="flex flex-row w-full justify-end">
@@ -61,14 +68,14 @@ export default function Home() {
                     className="bg-blue-400 rounded-3xl p-3 m-3 w-1/6 text-center  hover:bg-gray-200"
                     onClick={() => setMenuDishes()}>
                     Test Regenerate Menu
-                </NavLink> 
+                </NavLink>
             </div>
             <div className="flex flex-row w-full justify-end mb-1">
-            <FileUploadIcon onClick={downloadMenuPDF} className="cursor-pointer" />
+                <FileUploadIcon onClick={downloadMenuPDF} className="cursor-pointer" />
             </div>
-            
 
             <div className="flex flex-col gap-10">
+                <ToastContainer />
                 {/* Current Week menu */}
                 <WeeklyMenu weekStartDay={moment().startOf("isoWeek")} dishes={menuDishes.currentWeek} />
 
