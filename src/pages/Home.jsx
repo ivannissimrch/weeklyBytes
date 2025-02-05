622ae3f1ce266f1be2bec299ad499803a50ca909
Jun Kyung Lee <junlee0325@gmail.com>
2025-02-05 13:06:01 -0500

    Logo directs to Home. Responsive css in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,10 @@ import generatePDF from "../functions/generatePDF";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-col w-full items-end justify-end">
+    <div className="flex flex-col items-center w-screen px-4 ">
+      <div className="flex flex-col w-full md:items-center items-end justify-center">
         <NavLink
-          className="flex flex-row justify-center items-center bg-button-blue border-2 text-white rounded-3xl h-[36px] w-fit px-[30px]  my-2 hover:bg-white hover:text-button-blue hover:border-2 hover:border-button-blue"
+          className="flex flex-row justify-center items-center bg-button-blue border-2 text-white rounded-3xl h-[36px] w-fit px-[18px] md:px-[60px] my-2 hover:bg-white hover:text-button-blue hover:border-2 hover:border-button-blue"
           to={"/GenerateMenu"}
         >
           <AddIcon fontSize="small" className=""/>
@@ -20,7 +20,16 @@ export default function Home() {
         </NavLink>
         <div
           onClick={generatePDF}
-          className="flex flex-row bg-gray-500 border-2 text-white justify-center items-center rounded-full h-[36px] w-fit px-[24px] py-[6px] hover:border-2 hover:border-solid hover:border-gray-500 hover:bg-white hover:text-gray-500 cursor-pointer"
+          className="md:hidden flex flex-row text-button-blue justify-center items-center w-fit border-2 border-button-blue aspect-square rounded-full p-[2px]"
+        >
+          <FileUploadIcon
+            fontSize="medium"
+            className="cursor-pointer"
+          />
+        </div>
+        <div
+          onClick={generatePDF}
+          className="hidden md:flex md:flex-row md:bg-gray-500 md:border-2 md:text-white md:justify-center md:items-center md:rounded-full md:h-[36px] md:w-fit md:px-[24px] md:py-[6px] md:hover:border-2 md:hover:border-solid md:hover:border-gray-500 md:hover:bg-white md:hover:text-gray-500 md:cursor-pointer"
         >
           <span>Export Menu</span>
           <FileUploadIcon
@@ -30,7 +39,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col items-center w-full ">
         <ToastContainer />
         {/* Current Week menu */}
         <WeeklyMenu
