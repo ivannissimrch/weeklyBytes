adc99352fb9aee13327171212fbb3b7b93e520a6
Jun Kyung Lee <junlee0325@gmail.com>
2025-02-12 13:41:59 -0500

    Footer containing team repo created

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -8,6 +8,10 @@ export default function RootLayout() {
             <main className="flex flex-col items-center w-full bg-[url(/bg.jpg)] bg-cover h-fit">
                 <Outlet />
             </main>
+            <div className="bg-button-yellow w-screen h-fit flex items-center py-2 px-5 hover:underline">
+                <img className="aspect-square w-5" src="./github-mark.svg" />
+                <a href="https://github.com/chingu-voyages/V53-tier2-team-26">&nbsp;GitHub | chingu-v53-tier2-team26</a>
+            </div>
         </div>
     );
 }
